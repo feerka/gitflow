4b920afbe22fd3d34031bd1d0aff4dbccf9257c5
Fernando Casique <feerka.eng@gmail.com>
2023-03-28 12:08:26 +0200

    Se realizan cambios en x

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ console.log('Se realiza la hu planteada, se agregan cambios de verisonamiento en
 
 
 console.log('Se realiza la hu 2222 planteada, se agregan cambios de verisonamiento en el changelog v2');
+
+console.log('Se realizan las correciones de los errores encontrados en release de la hu 2222 planteada y el fet xxxx');
